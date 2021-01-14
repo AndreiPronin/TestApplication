f79f98daf02dc60b02ec4dba855769c86bdc58a9
AndreiPronin <andrei985623147@yandex.ru>
2021-01-14 16:53:21 +0300

    Добавьте файлы проекта.

---
+++ TestApplication/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+namespace TestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseTestEntities db = new DatabaseTestEntities();
+        public ActionResult Index()
+        {
+            List<Ofice> ofices = db.Ofice.ToList();
+            ViewBag.P1 = new SelectList(ofices.ToList(), "Id","Name");
+            return View();
+        }
+        
+        public ActionResult GetTimeSlots(int officeId , DateTime DateAndTime)
+        {
+            DateTime date = DateAndTime;
+            date += new TimeSpan(23, 59, 59);
+            List<TimeSlot> timeslot = db.TimeSlot.Where(x => x.OfficeId == officeId && (x.BeginTime >= DateAndTime && x.EndTime <= date)).ToList();
+            return PartialView(timeslot);
+        }
+        public ActionResult Arrange(int officeId, string DateAndTime)
+        {
+            DateTime begindate = Convert.ToDateTime(DateAndTime);
+            DateTime enddate = begindate + new TimeSpan(0,30,0);
+            db.TimeSlot.Add(new TimeSlot { BeginTime = begindate, EndTime = enddate, IsBusy = true, OfficeId = officeId });
+            db.SaveChanges();
+           
+            return Content("Вы успешно записались на " + DateAndTime);
+        }
+        public ActionResult Cancel(int officeId, string DateAndTime)
+        {
+            DateTime begindate = Convert.ToDateTime(DateAndTime);
+            DateTime enddate = begindate + new TimeSpan(0, 30, 0);
+            var Cansel = db.TimeSlot.Where(x => x.OfficeId == officeId && x.BeginTime == begindate).ToList().FirstOrDefault();
+            db.TimeSlot.Remove(Cansel);
+            db.SaveChanges();
+
+            return Content("Вы успешно отменили запись на " + DateAndTime);
+        }
+    }
+}
---
+++ TestApplication/Controllers/OficesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TestApplication;
+
+namespace TestApplication.Controllers
+{
+    public class OficesController : ApiController
+    {
+        private DatabaseTestEntities db = new DatabaseTestEntities();
+
+      
+        public IQueryable<Ofice> GetOfice()
+        {
+            return db.Ofice;
+        }
+        public IQueryable<TimeSlot> GetTimeSlots(int officeId, DateTime date)
+        {
+            DateTime DateAndTime = date;
+            DateAndTime += new TimeSpan(23, 59, 59);
+            return db.TimeSlot.Where(x => x.OfficeId == officeId && (x.BeginTime >= date && x.EndTime <= DateAndTime));
+        }
+
+    }
+}
